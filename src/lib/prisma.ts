47e4a73df9d29d84669d6a1b890763a37c0c9dcd
Sumanta <sumantablog@gmail.com>
2024-04-02 13:56:45 +0530

    Add Prisma client and utility function

---
+++ src/lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+/**
+ * Represents a global variable for Prisma.
+ * @name globalForPrisma
+ * @type {Object}
+ */
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Generates a random ID.
+ * @returns A string representing the generated ID.
+ */
+export function idGenerator(): string {
+  return Math.floor(Math.random() * 10001).toString();
 }
